7543359b7c5cce5356c9321a6fc14937f3edcad0
dubiao <191562687@qq.com>
2016-12-19 15:01:26 +0800

    注册
    注册

--- JAJ/Classes/主要/我/C/ChangePassWordViewController.m
+++ JAJ/Classes/主要/我/C/ChangePassWordViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ChangePassWordViewController.h"
+#import "UserInfoManager.h"
 
 @interface ChangePassWordViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *textPassword;
+@property (strong, nonatomic) IBOutlet UITextField *textNEWPAS1;
+@property (strong, nonatomic) IBOutlet UITextField *textNEWPAS2;
 
 @end
 
@@ -17,39 +21,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"修改密码";
+    
+
+}
+- (IBAction)changePassWord:(id)sender {
+    
+    /** 密码修改 */
+    [self getData];
+    
 }
 
 - (void)getData
 {
     
+    if (self.textPassword.text.length < 1) {
+        [self showErrorTips:@"密码不能为空"];
+        return;
+    }
+    if (self.textNEWPAS1.text.length < 1) {
+        [self showErrorTips:@"密码不能为空"];
+        return;
+    }
+    if (self.textNEWPAS2.text.length < 1) {
+        [self showErrorTips:@"密码不能为空"];
+        return;
+    }
+
+    
     NSString *url = [NSString stringWithFormat:@"%@%@",ip_port,appShiyts_URL];
 //    请求参数：json={USERNAME :"san",PASSWORD:"1",NEWPAS1:"2",NEWPAS2:"2"}
 //    用户名		密码		新密码		确认密码
-    NSDictionary *params = @{
-                             @"USERNAME":@"20",
-                             @"PASSWORD":@"20",
-                             @"NEWPAS1":@"20",
-                             @"NEWPAS2":@"20"
-                             };
     
+    UserModel * model = [UserInfoManager getUserInfo];
+    
+    NSDictionary *params = @{
+                             @"USERNAME":model.USERNAME,
+                             @"PASSWORD":self.textPassword.text,
+                             @"NEWPAS1":self.textNEWPAS1.text,
+                             @"NEWPAS2":self.textNEWPAS2.text
+                             };
+
     NSString *p1Str = [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:params options:0 error:nil] encoding:NSUTF8StringEncoding];
     NSDictionary *json = @{@"json":p1Str};
     
+    NSLog(@"json = %@",json);
+    
     [HTTPManager POST:url params:json success:^(NSURLSessionDataTask *task, id responseObject) {
 
+        NSLog(@"responseObject = %@",responseObject);
         
-        //     NSLog(@"responseObject = %@",responseObject);
         NSString *code = [[responseObject objectForKey:@"code"] description];
         NSString *desc = [[responseObject objectForKey:@"desc"] description];
         if ([code isEqualToString:@"0000"]) {
             // 成功
-            
-//            NSArray *data = [responseObject objectForKey:@"data"];
-//            self.hangyeArray = [HomeListModel mj_objectArrayWithKeyValuesArray:data];
-//            
-//            [self.tableView reloadData];
+            [self showErrorTips:@"修改成功"];
+            [self.navigationController popViewControllerAnimated:YES];
 
-            
         }else{
             // 不成功
             [self sendAlertAction:desc];
